30e28dc7f1620abaa5068480bde2bd6bc0a6bb39
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-20 09:18:53 -0500

    creating initial views

---
+++ PersonalDiary/Controllers/EntryController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalDiary.Controllers
+{
+    public class EntryController : Controller
+    {
+        // GET: Entry
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Entry/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Entry/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Entry/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Entry/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Entry/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Entry/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Entry/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
