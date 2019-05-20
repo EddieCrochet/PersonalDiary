aaecc09ddfddd823ce4a720ac5fbadeb4c486466
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-20 09:05:34 -0500

    Added my Entry class

---
+++ PersonalDiary/Models/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalDiary.Models
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        public Entry(int id, string text, DateTime date)
+        {
+            this.Id = id;
+            this.Text = text;
+            this.Date = date;
+        }
+    }
+}
