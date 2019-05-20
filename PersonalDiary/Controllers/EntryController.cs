6eb4b3fc28b663aa56703470c851b45d28901ad9
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-20 09:39:36 -0500

    beginning set up of DB with EF through views

--- PersonalDiary/Controllers/EntryController.cs
+++ PersonalDiary/Controllers/EntryController.cs
@@ -2,92 +2,151 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PersonalDiary.Models;
 
 namespace PersonalDiary.Controllers
 {
     public class EntryController : Controller
     {
-        // GET: Entry
-        public ActionResult Index()
+        private readonly EntryContext _context;
+
+        public EntryController(EntryContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: Entry
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Entry.ToListAsync());
         }
 
         // GET: Entry/Details/5
-        public ActionResult Details(int id)
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entry
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
         }
 
         // GET: Entry/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
 
         // POST: Entry/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create([Bind("Id,Text,Date")] Entry entry)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
-
+                _context.Add(entry);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(entry);
         }
 
         // GET: Entry/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entry.FindAsync(id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+            return View(entry);
         }
 
         // POST: Entry/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Text,Date")] Entry entry)
         {
-            try
+            if (id != entry.Id)
             {
-                // TODO: Add update logic here
+                return NotFound();
+            }
 
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(entry);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EntryExists(entry.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(entry);
         }
 
         // GET: Entry/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entry
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
         }
 
         // POST: Entry/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            try
-            {
-                // TODO: Add delete logic here
+            var entry = await _context.Entry.FindAsync(id);
+            _context.Entry.Remove(entry);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+        private bool EntryExists(int id)
+        {
+            return _context.Entry.Any(e => e.Id == id);
         }
     }
 }
--- PersonalDiary/Startup.cs
+++ PersonalDiary/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using PersonalDiary.Models;
 
 namespace PersonalDiary
 {
@@ -33,6 +35,10 @@ namespace PersonalDiary
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<EntryContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("EntryContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
