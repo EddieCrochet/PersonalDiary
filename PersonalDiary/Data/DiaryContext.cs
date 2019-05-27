3bc4d43a367b116fabebef87c44f02476305b3b5
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-27 15:37:07 -0500

    added a dbcontext class

---
+++ PersonalDiary/Data/DiaryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalDiary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalDiary.Data
+{
+    public class DiaryContext : DbContext
+    {
+        public DiaryContext(DbContextOptions<DiaryContext> options) : base (options)
+            {
+            }
+
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<Diary> Diaries { get; set; }
+    }
+}
