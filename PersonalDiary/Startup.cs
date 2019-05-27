25f57b647f5f3e185b29ff6400dfcc84d0c7c1b0
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-27 15:43:29 -0500

    Registered my dbconext with to the database with this startup.configure method

--- PersonalDiary/Startup.cs
+++ PersonalDiary/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PersonalDiary.Data;
 
 namespace PersonalDiary
 {
@@ -31,6 +33,8 @@ namespace PersonalDiary
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DiaryContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
