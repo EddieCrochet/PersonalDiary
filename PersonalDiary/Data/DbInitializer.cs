632f3aefcf6cbdc9b1239318e99594f4b4e898ef
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-05-27 16:00:52 -0500

    Added my database initializer

---
+++ PersonalDiary/Data/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using PersonalDiary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalDiary.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(DiaryContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if(context.Entries.Any())
+            {
+                return; //db has been seeded or has data
+            }
+
+            var entries = new Entry[]
+            {
+                new Entry{Date=DateTime.Parse("05/16/2019 05:55PM"),
+                    Text ="Wowzers, it's my bday" }
+            };
+
+            foreach(Entry e in entries)
+            {
+                context.Entries.Add(e);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
