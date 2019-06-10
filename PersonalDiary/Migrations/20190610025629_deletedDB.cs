7f88efcd5ef5416a4d593dd10bb2a2a41b1778d7
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-06-10 10:20:55 -0500

    deleted some migrations I had but dont need

---
+++ PersonalDiary/Migrations/20190610025629_deletedDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersonalDiary.Migrations
+{
+    public partial class deletedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Diaries",
+                table: "Diaries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Diaries",
+                table: "Diaries",
+                column: "DiaryID")
+                .Annotation("SqlServer:Clustered", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Diaries",
+                table: "Diaries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Diaries",
+                table: "Diaries",
+                column: "DiaryID");
+        }
+    }
+}
